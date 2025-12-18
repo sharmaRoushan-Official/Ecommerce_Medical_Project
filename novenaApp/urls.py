98623b238d8261fd8a278297c683784ade32b8be
sharmaRoushan-Official <roushankr26@gmail.com>
2025-12-18 12:01:22 +0530

    working on about and services

--- novenaApp/urls.py
+++ novenaApp/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('home/',viewHome), # http://127.0.0.1:8000/novapp/home/
+    path('home/',viewHome, name='home'), # http://127.0.0.1:8000/novapp/home/
+    path('about/',viewAbout,name='about'), 
+    path('service/',viewService,name="service"),
     
 ]
