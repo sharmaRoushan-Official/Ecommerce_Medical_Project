a51ffa90198cbc5797a399e0b76288cdf38d7346
sharmaRoushan-Official <roushankr26@gmail.com>
2025-12-20 17:04:03 +0530

    third commit

--- novenaApp/urls.py
+++ novenaApp/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
     path('home/',viewHome, name='home'), # http://127.0.0.1:8000/novapp/home/
     path('about/',viewAbout,name='about'), 
     path('service/',viewService,name="service"),
+    path('department/',viewDepartment,name='department'),
+    path('singleDepartment/',viewSingleDepartment,name='singleDepartment'),
+    path('doctors/',viewDoctors,name='doctors'),
+    path('singleDoctor/',viewSingleDoctor,name='singleDoctor'),
+    path('appoinment/',viewAppoinment,name='appoinment'),
+    path('blog/',viewBlog,name='blog'),
+    path('singleBlog/',viewSingleBlog,name='singleBlog'),
+    path('contact/',viewContact,name='contact'),
     
 ]
--- novenaApp/views.py
+++ novenaApp/views.py
@@ -10,6 +10,43 @@ def viewHome(request):
 def viewAbout(request):
     resp = render(request, 'novenaApp/about.html')
     return resp
+
+
 def viewService(reqeust):
     resp = render(reqeust,"novenaApp/service.html")
+    return resp
+
+
+def viewDepartment(request):
+    resp = render(request,'novenaApp/department.html')
+    return resp
+
+def viewSingleDepartment(request):
+    resp = render(request,'novenaApp/singleDepartment.html')
+    return resp
+
+
+def viewDoctors(request):
+    resp = render(request,'novenaApp/doctors.html')
+    return resp
+
+def viewSingleDoctor(reqeust):
+    resp = render(reqeust,"novenaApp/singleDoctor.html")
+    return resp
+
+def viewAppoinment(request):
+    resp = render(request,"novenaApp/appoinment.html")
+    return resp
+
+
+def viewBlog(request):
+    resp = render(request, 'novenaApp/blog.html')
+    return resp
+
+def viewSingleBlog(request):
+    resp  = render(request,'novenaApp/singleBlog.html')
+    return resp
+
+def viewContact(request):
+    resp = render(request,"novenaApp/contact.html")
     return resp
